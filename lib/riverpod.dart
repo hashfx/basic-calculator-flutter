b0a9484bdcaae2e9a0d65bc1d4458dd4dbf00554
hashfx <algebra.soni@gmail.com>
2022-09-23 21:41:42 +0530

    modify screen data with appended data

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -8,4 +8,16 @@ final calculatorProvider =
 
 class CalculatorNotifier extends StateNotifier<Calculator> {
   CalculatorNotifier() : super(Calculator());
+
+  // executes every time user click buttons
+  void append(String buttonText) {
+    // get equation
+    final equation = () {
+      // append current text to new text
+      // if screen has 0, replace 0 by new text
+      return state.equation == '0' ? buttonText : state.equation + buttonText;
+    }();
+    // change equation on input area and override with new value
+    state = state.copy(equation: equation);
+  }
 }

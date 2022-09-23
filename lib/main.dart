639a6483822ef9ab2d1526b4e018792547475592
hashfx <algebra.soni@gmail.com>
2022-09-23 19:35:02 +0530

    main.dart completed

--- lib/main.dart
+++ lib/main.dart
@@ -39,24 +39,81 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Container(
-          margin: EdgeInsets.only(left: 8),
-          child: Text(widget.title),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Container(
+            margin: EdgeInsets.only(left: 8),
+            child: Text(widget.title),
+          ),
         ),
-      ),
-      body: SafeArea(
+        body: SafeArea(
           child: Column(
-        children: <Widget>[
-          Expanded(child: Container()),
-          Expanded(flex: 2, child: buildButtons())
-        ],
-      )),
-    );
+            children: <Widget>[
+              Expanded(child: buildResult()),
+              Expanded(flex: 2, child: buildButtons())
+            ],
+          ),
+        ),
+      );
 
+  Widget buildResult() => Container(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              '0',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Colors.white, fontSize: 36),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              '0',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(color: Colors.grey, fontSize: 18),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildButtons() => Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: MyColors.background2,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+        ),
+        child: Column(
+          children: <Widget>[
+            buildButtonRow('AC', '<', '', '÷'),
+            buildButtonRow('7', '8', '9', '⨯'),
+            buildButtonRow('4', '5', '6', '-'),
+            buildButtonRow('1', '2', '3', '+'),
+            buildButtonRow('0', '.', '', '='),
+          ],
+        ),
+      );
+
+  Widget buildButtonRow(
+    String first,
+    String second,
+    String third,
+    String fourth,
+  ) {
+    final row = [first, second, third, fourth];
+
+    return Expanded(
+      child: Row(
+        children: row
+            .map((text) => ButtonWidget(
+                  text: text,
+                  onClicked: () => print(text),
+                  onClickedLong: () => print(text),
+                ))
+            .toList(),
+      ),
+    );
   }
 }
